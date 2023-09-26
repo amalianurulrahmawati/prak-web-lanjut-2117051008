4fb36ccaba8a6fe98323e8a521f95cb66bd59887
amalianurulrahmawati <amelianurulrahmawati@gmail.com>
2023-09-26 09:25:44 +0700

    update

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -3,21 +3,43 @@
     <head>
         <title>Halaman Login</title>
         <link rel="stylesheet" href="<?=base_url('assets/css/style.css');?>">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
    
     <body>
         <div class="container">
             <form action="<?= base_url('/user/store')?>"method="post">
-            <form>
+            
                 <label>Nama</label><br>
                 <input type="text" name="nama"><br>
                 <label>Kelas</label><br>
-                <input type="text" name="kelas"><br>
-                <label>NPM</label><br>
-                <input type="text" name="npm"><br>
-                <button>Submit</button>
+                <select type="text" name="kelas">
+                    <?php
+                    foreach ($kelas as $item){
+                    ?>
+                        <option value="<?= $item['id'] ?>">
+                            <?= $item['nama_kelas'] ?>
+                        </option>
+                    <?php
+                    }
+                    ?>
+                </select>
+                <br><br>
+
+                <!-- <label>NPM</label><br>
+                <input type="text" name="npm"><br> -->
+                
+
+                <input type="" name="npm" class="form-control <?= ($validation->hasError('npm')) ? 'is-invalid' : '' ; ?>" id="npm">
+                            <div class="invalid-feedback">
+                                <?= $validation->getError('npm'); ?>
+                </div>
+
+
+                                <button type="submit">Submit</button>
+                            
             </form>
-            </form>
+            
         </div>     
     </body>
 </html>
